6b9b8e23d68dc1ba96e5234385d603d854141121
stevan06v <stevanvlajic5@gmail.com>
2024-07-19 19:37:53 +0200

    init: initialized main file-structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# NEPTUNCLI
+
+## Useful
+* https://realpython.com/python-typer-cli/ -> project-structure & explanation
---
+++ neptun/__main__.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def main():
+    print("NEPTUN-CLI started successfully...")
+
+
+if __name__ == "__main__":
+    main()
